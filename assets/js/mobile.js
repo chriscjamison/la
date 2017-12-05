6d13f5e38eb409977add92227781e13dd25d32b6
Chris Jamison <chris@chriscjamison.com>
2017-12-04 21:42:29 -0800

    0.3.1 - Contact Us page complete thru first pass * Korean character display issues cleared up

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,19 +1,8 @@
-function loadDesktopPage ()  {
-  
-  /*  ---- ---- ---- FUNCTION OUTLINE ---- ---- ---- ----
-          Function loads the HTML content of the webpage
-
-      ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- */
-
-  // Initialize variable which will contain the HTML.
-  var html_content;
-
-  // Store HTML inside a variable, 'html_content'.
-  html_content = jq(html).html;
-
-  // Send the HTML to be parsed by another function.
-  return html_content;
-} // END of FUNCTION 'loadDesktopPage'
+jq(window).load(
+  function () {
+    jq("body").css("opacity", 0);
+  }
+);
 
 function extractHtmlFromDesktopPage ()  {
 
@@ -22,28 +11,119 @@ function extractHtmlFromDesktopPage ()  {
           the HTML.
 
       ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- */
+  
+  jq(document).ready(
+    function () {
+      // Initialize variable to hold content of page.
+      var html_content = new String();
+      var map_content = new String();
 
-  // Initialize variable to hold content of page.
-  var html_content = new String();
+      // Initialize variable to hold URL string.
+      var url_string = new String();
 
-  // Store relavent content in 'html_content'.
-  html_content = jq("table:nth-child(2) table:nth-child(2) table:nth-child(1)").html;
-  jq("html").html(
-    "<!DOCTYPE html>/n" + 
-    "<html lang=\"en\">/n" + 
-    "  <head>\n" + 
+      // Initialize array to hold strings which correspond to the URL's of the webpage
+      // which the browser loads.
+      var urls_Array = new Array;
+
+      // Initialize number which matches the index within 'urls_Array' for a given 
+      // webpage.
+      webpage_index = new Number();
+
+      // Pass on the URL's of the pages within the website.
+      urls_Array = [
+        'contact_us.htm', 
+        'history.htm'
+      ];
+
+      // Pass on the URL string of the loading page.
+      url_string = window.location.href;
+
+      jq.each(urls_Array, 
+        function (index, value) {
+          var current_URL = value;
+          
+          if (url_string.indexOf(current_URL) > -1) {
+            switch (index)  {
+              case 0: 
+                var removed_HTML = new String();
+
+                // jq("table table:nth-child(2) > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(2) > td table > tbody > tr:nth-child(2) > td").html("");
+                // removed_HTML = jq("table tbody > tr > td.bg_tops > table.aleft > tbody > tr > td > table + table   > tbody > tr + tr > td > table > tbody > tr").detach();
+                
+
+                html_content = jq("table.aleft > tbody > tr > td > table + table > tbody > tr + tr > td").html();
+
+                webpage_index = 0;
+                
+              break;
+              
+            }
+          }
+        }
+      );
+              
+      // Make the content of the Desktop site invisible to the visitor.
+      jq("body").fadeOut(10);
+
+      // Begin the process to display the content of the Desktop site within 
+      // the Mobile template.
+      setupFinalContent(html_content, webpage_index);
+    }
+  );
+} // END of FUNCTION 'extractHtmlFromDesktopPage'
+
+function setupFinalContent(html_content, webpage_index)  {
+
+  // Clear out the HTML of the browser.
+  jq("html").empty();
+
+  // Add the basic framework of the HTML template.
+  renderHTMLFrame();
+
+  // Add the HTML of the <head> HTML element.
+  renderHeader();
+  
+  // Add the basic framework of the <body> HTML element.
+  renderBody();
+
+  // Add the HTML which makes up the logo, search, and login.
+  renderLogoAndSearch();
+
+  // Add the HTML which makes up the footer.
+  renderFooter();
+
+  // Add the HTML of the page which is loading.
+  renderArticle(html_content, webpage_index);
+
+} // END of FUNCTION 'setupFinalContent'
+
+function renderHTMLFrame()  {
+  jq("html").removeAttr();
+  jq("html").attr("lang", "ko");
+
+  jq("html").html( 
+    "  <head>\n" +
+    "  </head>\n" + 
+    "  <body>" + 
+    "  </body>\n"
+  );
+} // END of FUNCTION 'renderHTMLFrame'
+
+function renderHeader() {
+  jq("head").html(
     "    <!-- *** Page Title *** -->\n" + 
     "    <title>Leaders Academy - Contact Us</title>\n" + 
     "\n" + 
     "    <!-- HTML Reference to display Korean characters -->\n" + 
-    "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n" + 
+    "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=euc-kr\">\n" + 
     "\n" + 
     "    <!-- *** CSS Stylesheet References *** -->\n" + 
     "\n" +
     "    <!-- Stylesheet which contains general layout CSS rules for 'index.htm'. -->\n" + 
-    "    <link rel=\"stylesheet\" href=\"assets/css/mobile.css\">\n" + 
+    "    <link rel=\"stylesheet\" href=\"/la/assets/css/mobile.css\">\n" + 
+    /* 
     "    <!-- Stylesheet which contains CSS rules specifically for the English language version. -->\n" + 
-    "    <link rel=\"stylesheet\" href=\"assets/css/en.css\">\n" + 
+    "    <link rel=\"stylesheet\" href=\"/la/assets/css/en.css\">\n" +  */
     "\n" +
     "    <!-- *** JavaScript file references *** -->\n" + 
     "\n" +
@@ -51,11 +131,15 @@ function extractHtmlFromDesktopPage ()  {
     "    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>\n" + 
     "\n" +
     "    <!-- JavaScript script which handles general layout for 'index.htm'. -->\n" + 
-    "    <script src=\"assets/js/mobile.js\"></script>\n" + 
-    "  </head>\n" + 
-    "  <body>" + 
+    "    <script src=\"/la/assets/js/mobile.js\"></script>\n"
+  );
+  
+} // END of FUNCTIION 'renderHeader'\
+
+function renderBody() {
+  jq("body").html(
     "<!--\n" +  
-    "  START of header\n" +  
+    "  START of Header\n" +  
     "        Contains:\n" + 
     "        + Logo\n" + 
     "        + Search box\n" + 
@@ -64,20 +148,7 @@ function extractHtmlFromDesktopPage ()  {
     "            - Korean language mobile page\n" + 
     "-->\n" + 
     "    <header>\n" + 
-    "      <img src=\"assets/img/logo/logo.png\" width=\"230\" height=\"170\" alt=\"Leaders Academy\" />\n" + 
-    "      <section id=\"header-right\">\n" + 
-    "        <div id=\"header-search\">\n" + 
-    "        <form>\n" + 
-    "          <input type=\"search\" width=\"25\" maxwidth=\"75\" value=\"Search\" id=\"mobile-search\">\n" + 
-    "        </form>\n" + 
-    "        </div>\n" + 
-    "        <div id=\"header-links\">\n" + 
-    "          <a href=\"/login/\" title=\"Login\" id=\"link-login\">Login</a>\n" + 
-    "          <a href=\"/kr/\" title=\"한국어\" id=\"link-korean\">한국어</a>\n" + 
-    "        </div>\n" + 
-    "      </section>\n" + 
     "    </header>\n" + 
-    "\n" + 
     "<!--\n" +  
     "  START of Content\n" +  
     "    Contains:\n" + 
@@ -88,39 +159,204 @@ function extractHtmlFromDesktopPage ()  {
     "        - Directions\n" + 
     "-->\n" + 
     "    <article>\n" + 
-    "      " + html_content + "\n" +  
-    "    </article>\n" + 
+    "    </article>\n" +
     "<!--\n" +  
     "  START of Footer\n" +  
     "    Contains:\n" + 
     "      + Links to social media platforms\n" + 
     "      + Copyright information\n" + 
-    "-->\n" + 
-        <footer>
-          <section id="footer-border">
-            <div></div>
-          </section>
-          <section id="footer-row_1">
-            <a href="https://naver.com" title="Naver" target="_blank" id="link-naver"></a>
-            <a href="https://facebook.com" title="Facebook" target="_blank" id="link-facebook"></a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" id="link-twitter"></a>
-            <a href="https://youtube.com" title="YouTube" target="_blank" id="link-you_tube"></a>
-            <a href="https://kakao.com" title="Kakao" target="_blank" id="link-kakao"></a>
-          </section>
-          <section id="footer-row_2">
-            <a href="https://instagram.com" title="Instagram" target="_blank" id="link-instagram"></a>
-            <a href="https://wechat.com" title="WeChat" target="_blank" id="link-wechat"></a>
-            <a href="https://plus.google.com" title="Google+" target="_blank" id="link-google"></a>
-            <a href="https://line.com" title="Line" target="_blank" id="link-line"></a>
-            <a href="https://whatsapp.com" title="WhatsApp" target="_blank" id="link-whatsapp"></a>
-          </section>
-          <section id="footer-row_3">
-            <span>Leaders Academy &copy; 2017</span>
-          </section>
-        </footer>
-      </body>
-    </html>
-  )
+    "-->\n" +  
+    "    <footer>\n" + 
+    "    </footer>\n" 
+  );
+} // END of FUNCTION 'renderBody'
+
+function renderLogoAndSearch() {
+  jq("header").html(
+    "      <img src=\"/la/assets/img/logo/logo.png\" width=\"230\" height=\"170\" alt=\"Leaders Academy\" />\n" + 
+    "      <section id=\"header-right\">\n" + 
+    "        <div id=\"header-search\">\n" + 
+    "        <form>\n" + 
+    "          <input type=\"search\" width=\"25\" maxwidth=\"75\" value=\"Search\" id=\"mobile-search\">\n" + 
+    "        </form>\n" + 
+    "        </div>\n" + 
+    "        <div id=\"header-links\">\n" + 
+    "          <a href=\"/login/\" title=\"Login\" id=\"link-login\">Login</a>\n" + 
+    "          <a href=\"/kr/\" title=\"한국어\" id=\"link-korean\">한국어</a>\n" +  
+    "        </div>\n" + 
+    "      </section>\n"
+  );
+} // END of FUNCTION 'renderLogoAndSearch'
+
+function renderFooter() {
+  jq("footer").html(
+    "      <section id=\"footer-border\">\n" + 
+    "        <div></div>\n" + 
+    "      </section>\n" + 
+    "      <section id=\"footer-row_1\">\n" + 
+    "        <a href=\"https://naver.com\" title=\"Naver\" target=\"_blank\" id=\"link-naver\"></a>\n" + 
+    "        <a href=\"https://facebook.com\" title=\"Facebook\" target=\"_blank\" id=\"link-facebook\"></a>\n" + 
+    "        <a href=\"https://twitter.com\" title=\"Twitter\" target=\"_blank\" id=\"link-twitter\"></a>\n" + 
+    "        <a href=\"https://youtube.com\" title=\"YouTube\" target=\"_blank\" id=\"link-you_tube\"></a>\n" + 
+    "        <a href=\"https://kakao.com\" title=\"Kakao\" target=\"_blank\" id=\"link-kakao\"></a>\n" + 
+    "      </section>\n" + 
+    "      <section id=\"footer-row_2\">\n" + 
+    "        <a href=\"https://instagram.com\" title=\"Instagram\" target=\"_blank\" id=\"link-instagram\"></a>\n" + 
+    "        <a href=\"https://wechat.com\" title=\"WeChat\" target=\"_blank\" id=\"link-wechat\"></a>\n" + 
+    "        <a href=\"https://plus.google.com\" title=\"Google+\" target=\"_blank\" id=\"link-google\"></a>\n" + 
+    "        <a href=\"https://line.com\" title=\"Line\" target=\"_blank\" id=\"link-line\"></a>\n" + 
+    "        <a href=\"https://whatsapp.com\" title=\"WhatsApp\" target=\"_blank\" id=\"link-whatsapp\"></a>\n" + 
+    "      </section>\n" + 
+    "      <section id=\"footer-row_3\">\n" + 
+    "        <span>Leaders Academy &copy; 2017</span>\n" + 
+    "      </section>\n"
+  );
+} // END of FUNCTION 'renderFooter'
+
+function renderArticle(html_content, webpage_index) {
+  jq("article").html(html_content);
+
+  switch (webpage_index)  {
+    case 0: 
+      jq("article > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(1)").html(
+        "<div>Location</div>\n" + 
+        "<a href=\"http://map.daum.net/?urlX=511366&urlY=1111406&urlLevel=3&map_type=TYPE_MAP&map_hybrid=false&SHOWMARK=true\" target=\"_blank\">\n" + 
+        "  <span style=\"background:#000;position:absolute;width:557px;opacity:.7;filter:alpha(opacity=70);color:#fff;overflow:hidden;font:12px/1.5 Dotum, '����', sans-serif;text-decoration:none;padding:7px 0px 0px 10px; height: 24px;\">������ Ŭ���Ͻø� ��ġ������ Ȯ���Ͻ� �� �ֽ��ϴ�.</span><img width=\"565\" height=\"308\" src=\"http://map2.daum.net/map/mapservice?MX=511366&MY=1111406&SCALE=2.5&IW=565&IH=308&COORDSTM=WCONGNAMUL\" style=\"border:1px solid #ccc\">\n" + 
+        "</a>\n"
+      );
+    break;
+  }
+
+  formatCSS();
   
-  jq("article").html()
-} // END of FUNCTION 'extractHtmlFromDesktopPage'
+  jq("body").fadeIn();
+} // END of FUNCTION 'renderArticle'
+
+function formatCSS() {
+    /*  ---- ---- ---- FUNCTION OUTLINE ---- ---- ---- ----
+        Function formats the CSS properties of the 
+        extracted HTML.
+    ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- */
+
+    // Initialize variables to hold HTML selectors.
+    var table_selector = new String();
+    var td_selector = new String();
+    var div_selector = new String();
+    var sj_title_sj_contents_selector = new String();
+    var sj_title_selector = new String();
+    var sj_contents_selector = new String();
+    var top_table_top_cell_selector = new String();
+    var top_table_bottom_two_cells_selector = new String();
+    var address_cell_selector = new String();
+    var third_cell_selector = new String();
+    var subway_cell_selector = new String();
+
+    // Initialize variables to hold CSS values.
+    var table_css = new Object();
+    var td_css = new Object();
+    var div_css = new Object();
+    var sj_title_sj_contents_css = new Object();
+    var sj_title_css = new Object();
+    var sj_contents_css = new Object();
+    var top_table_top_cell_css = new Object();
+    var top_table_bottom_two_cells_selector = new Object();
+    var address_cell_css = new Object();
+    var third_cell_css = new Object();
+    var subway_cell_css = new Object();
+
+    // Pass on CSS values to variables.
+    
+    /* For HTML tags using the selector, '<table>'. */
+    table_css = {
+      width: "100%"
+    };
+
+    /* For HTML tags using the selector, '<td>'. */
+    td_css = {
+      padding: "0.2em 0", 
+      verticalAlign: "top", 
+      fontSize: "28pt"
+    };
+
+    /* For HTML tags using the selector, 'article > div:nth-child(1)'. */
+    div_css = {
+      marginBottom: "0.25em", 
+      fontFamily: "Arial, Helvetica, sans-serif", 
+      fontSize: "1.5em", 
+      fontWeight: "600"
+    };
+
+    /* For HTML tags using the selectors, '.sj_title' and '.sj_contents'. */
+    sj_title_sj_contents_css = {
+      fontFamily: "Arial, Helvetica, sans-serif", 
+      lineHeight: "28pt"
+    };
+
+    /* For HTML tags using the selector, '.sj_title'. */
+    sj_title_css = {
+      width: "25%", 
+      fontWeight: "600"
+    };
+
+    /* For HTML tags using the selector, '.sj_contents'. */
+    sj_contents_css = {
+      width: "75%"
+    };
+
+    /* For the top cell of the top table in the Location page. */
+    top_table_top_cell_css = {
+      fontSize:  "11pt"
+    };
+
+    /* For the bottom two cells of the top table in the Location page. */
+    top_table_bottom_two_cells_css = {
+      fontSize:  "25pt"
+    };
+
+    /* For the 'Address' cell of the table.. */
+    address_cell_css = {
+      fontSize: "25pt"
+    };
+    
+    /* For the last cell on the right of the table above the table used to 
+        display the directions. */
+    third_cell_css = {
+      fontSize: "11pt"
+    };
+    
+    /* For the cell of the bottom table which offers directions from the 
+        subway. */
+    subway_cell_css = {
+      fontSize: "11pt"
+    };
+
+    // Pass on the HTML selectors to variables.
+    table_selector = "table";
+    td_selector = "td";
+    div_selector = "article > table > tbody > tr:nth-child(2) > td > div:nth-child(1)";
+    sj_title_sj_contents_selector = ".sj_title, .sj_contents";
+    sj_title_selector = ".sj_title";
+    sj_contents_selector = ".sj_contents";
+    top_table_top_cell_selector = "table table > tbody > tr:nth-child(2) > td:nth-child(2)";
+    top_table_bottom_two_cells_selector = "table table > tbody > tr:nth-child(4) > td:nth-child(2), " +
+                                          "table table > tbody > tr:nth-child(6) > td:nth-child(2)";
+    third_cell_selector = "article > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(6) > td:nth-child(2)";
+    address_cell_selector = "article > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(2) > td:nth-child(2)";
+    subway_cell_selector = "article > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(4) > td:nth-child(2), " + 
+                            "article > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(6) > td:nth-child(2), " + 
+                            "article > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(8) > td:nth-child(2)";
+
+    // HTML will be formatted using the variables holding the CSS values
+    jq(table_selector).css(table_css);
+    jq(td_selector).css(td_css);
+    jq(div_selector).css(div_css);
+    jq(sj_title_sj_contents_selector).css(sj_title_sj_contents_css);
+    jq(sj_title_selector).css(sj_title_css);
+    jq(sj_contents_selector).css(sj_contents_css);
+    jq(top_table_top_cell_selector).css(top_table_top_cell_css);
+    jq(top_table_bottom_two_cells_selector).css(top_table_bottom_two_cells_css);
+    // jq(third_cell_selector).css(third_cell_css);
+    jq(address_cell_selector).css(address_cell_css);
+    jq(subway_cell_selector).css(subway_cell_css);
+
+} // END of FUNCTION 'formatCSS'
